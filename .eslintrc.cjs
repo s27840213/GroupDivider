62b0d4d4c835353d8a545dc8fe8e4db1988c2a9f
張祐禎 <s27840213@gmail.com>
2023-03-08 11:31:47 +0800

    fix: fix the warning of no-used-var when pug interact with script setup variables

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,25 @@
 module.exports = {
-	parser: 'vue-eslint-parser',
+  parser: 'vue-eslint-parser',
 
-	parserOptions: {
-		parser: '@typescript-eslint/parser',
-		ecmaVersion: 2020,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
-	plugins: ['prettier'],
-	extends: [
-		'plugin:vue/vue3-recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue-pug/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier'
+  ],
 
-	rules: {
-		'prettier/prettier': 'error'
-	}
+  rules: {
+    'prettier/prettier': 'error'
+  }
 }
