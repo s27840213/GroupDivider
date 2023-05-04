0ee432294057df6959cf81cdf06181602c8d5d99
張祐禎 <s27840213@gmail.com>
2023-05-04 14:27:39 +0800

    fix: add base in vite.config.ts for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/GroupDivider/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
